d5507e10119531bb09a56934e8432b83211f896e
EdwardGold Edem Hogan <edge37@outlook.com>
2023-05-29 21:10:42 +0000

    .

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -10,6 +10,8 @@
 	import { page } from '$app/stores';
 	import { client_delete } from '$lib/util/client_del';
 	import TrashCan from 'carbon-icons-svelte/lib/TrashCan.svelte';
+	import Share from 'carbon-icons-svelte/lib/Share.svelte';
+	import CopyLinkButton from './CopyLinkButton.svelte';
 
 	let current_page: number, posts: Post[];
 
@@ -23,10 +25,7 @@
 {#if $page.data.session?.user === post.user}
 	<ButtonSet>
 		<Button on:click={() => goto(`${id}/edit`)} iconDescription="Edit" icon={Edit} />
-			<Button
-				icon={TrashCan}
-				on:click={async () => await client_delete(id)}
-			/>
+		<Button icon={TrashCan} on:click={async () => await client_delete(id)} />
 	</ButtonSet>
 {/if}
 
@@ -35,21 +34,39 @@
 	<p>Last Modified: {post.last_modified.toLocaleString}</p>
 </div> -->
 
-
-
 <!-- <Tab> -->
 <!-- <TabContent title="Post"> -->
 <div class="article">
 	<div class="title">
 		<h2>{post.name}</h2>
 		<CopyButton
-		feedback="The link to this post has been copied to the clipboard"
-		text={`${$page.url}`}
-		iconDescription="Copy the link to this post"
-	/>
+			feedback="This post has been copied to the clipboard"
+			text={`${post.name}\nBy ${post.user}\n\n${post.body}`}
+			iconDescription="Copy this post to the clipboard"
+		/>
+		{#if typeof navigator !== "undefined" && navigator.share && navigator.canShare()}
+			<Button
+				on:click={() =>
+					navigator.share({
+						url: $page.url.toString(),
+						text: `${post.name} by ${post.user}`,
+						title: `${post.name} by ${post.user}`
+					})}
+				icon={Share}
+				iconDescription="Share this article"
+			/>
+		{:else}
+			<CopyLinkButton
+				feedback="The link to this post has been copied to the clipboard"
+				text={$page.url.toString()}
+				iconDescription="Copy the link to this post to the clipboard"
+			/>
+		{/if}
 	</div>
+	<p>By {post.user}</p>
 	<p>{post.body}</p>
 </div>
+
 <!-- </TabContent> -->
 
 <!-- <TabContent title="Similar Posts">
@@ -62,6 +79,7 @@
 		display: flex
 		flex-direction: column
 		row-gap: 1rem
+
 	.title
 		display: flex
 		flex-direction: row
