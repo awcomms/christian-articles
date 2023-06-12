80e2821c91f3deb318f21ed180bb93bb16992994
EdwardGold Edem Hogan <edge37@outlook.com>
2023-06-12 19:09:22 +0000

    reply, inRedisArray

--- src/lib/util/post/reply.ts
+++ src/lib/util/post/reply.ts
@@ -1,13 +1,11 @@
+import { posts_index_name } from '$lib/constants';
+import { inRedisArray } from '../inRedisArray';
 import { client } from '../redis';
 
 export const reply = async ({ post, target }: { post: string; target: string }) => {
-	const replies: string[] = await client.json
-		.get(target, { path: '$.replies' })
-		.then((r) => JSON.parse(r)[0]);
-	if (!replies.includes(post)) client.json.arrAppend(target, '$.replies', [post]);
+	if (! await inRedisArray(posts_index_name, 'id', target, 'replies', post))
+		await client.json.arrAppend(target, '$.replies', [post]);
 
-	const replied: string[] = await client.json
-		.get(post, { path: '$.replied' })
-		.then((r) => JSON.parse(r)[0]);
-	if (!replied.includes(target)) client.json.arrAppend(post, '$.replied', [target]);
+	if (! await inRedisArray(posts_index_name, 'id', post, 'replied', target))
+		await client.json.arrAppend(post, '$.replied', [target]);
 };
--- src/lib/util/post/unreply.ts
+++ src/lib/util/post/unreply.ts
@@ -1,11 +1,13 @@
+import { posts_index_name } from '$lib/constants';
 import { get } from '../get';
+import { inRedisArray } from '../inRedisArray';
 import { client } from '../redis';
 import { shape } from '../shape';
 
 export const unreply = async ({ post, target }: { post: string; target: string }) => {
-	const { replies } = await get(target, ['$.replies']); //TODO-URGENT
-	if (replies.includes(post)) client.json.arrPop(target, '$.replies', replies.indexOf(post));
+	if (await inRedisArray(posts_index_name, 'id', target, 'replies', post))
+		client.json.arrPop(target, '$.replies', replies.indexOf(post));
 
-	const { replied } = await get(post, ['$.replied']);
-	if (replied.includes(target)) client.json.arrPop(post, '$.replied', replied.indexOf(target));
+	if (await inRedisArray(posts_index_name, 'id', post, 'replied', target))
+		client.json.arrPop(post, '$.replied', replied.indexOf(target));
 };
