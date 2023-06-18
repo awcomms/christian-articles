a6dffce21267898118026425fa82dd9fc50137ae
EdwardGold Edem Hogan <edge37@outlook.com>
2023-06-18 03:58:26 +0000

    r_description

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -4,6 +4,9 @@
 	import { Button, ButtonSet, Tab, TabContent, Tabs } from 'carbon-components-svelte';
 	import PostView from './PostView.svelte';
 	import type { Post } from '$lib/types';
+
+	const replies_description = post.replies_description || 'Articles belonging to this article';
+	const replied_description = post.replied_description || 'Articles this article belongs to';
 </script>
 
 <div>
@@ -15,13 +18,13 @@
 
 <ButtonSet stacked>
 	{#if should_pay}
-		<Button href="/post/{id}/replies">{post.replies_description}</Button>
+		<Button href="/post/{id}/replies">{replies_description || 'Child articles'}</Button>
 	{:else}
 		<Button href="/post/{id}/pay"
-			>Pay {post.payment.cost}NGN to access {post.replies_description}
+			>Pay {post.payment.cost}NGN to access {replies_description || 'Parent articles'}
 		</Button>
 	{/if}
-	<Button href="/post/{id}/replied">{post.replied_description}</Button>
+	<Button href="/post/{id}/replied">{replied_description}</Button>
 	<Button href="/post/{id}/similar">Similar posts</Button>
 </ButtonSet>
 
